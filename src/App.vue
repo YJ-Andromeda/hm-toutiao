679862b91ab6f13d69ab7cd346ee3cc4b157a296
magicstf <1339502170@qq.com>
2019-08-02 11:28:47 +0800

    使用element-ui和vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <div id="app">app</div>
+  <div id="app">
+    app
+    <el-button type="success">成功按钮</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "app"
-};
+  name: 'app'
+}
 </script>
 
 <style lang="less">
---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+// 初始化路由对象
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: []
+})
+export default router
